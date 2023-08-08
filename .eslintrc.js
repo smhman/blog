e63124095ec43ec9559bdeef6123045bca2068ac
sundei <76597738+smhman@users.noreply.github.com>
2023-08-08 17:21:09 +0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -42,5 +42,10 @@ module.exports = {
 		'react/jsx-curly-newline': 'off',
 		'@typescript-eslint/indent': 'off',
 		'jsx-quotes': 'off',
+		"indent": [2, "tab", { "SwitchCase": 1, "VariableDeclarator": 1 }],
+        "no-tabs": 0,
+        "react/prop-types": 0,
+        "react/jsx-indent": [2, "tab"],
+        "react/jsx-indent-props": [2, "tab"],
 	},
 };
