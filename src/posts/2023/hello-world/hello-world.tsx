207309af0470e6f871b668e6619126eb44c9609d
sundei <76597738+smhman@users.noreply.github.com>
2023-08-08 15:39:04 +0300

    Update hello-world.tsx

--- src/posts/2023/hello-world/hello-world.tsx
+++ src/posts/2023/hello-world/hello-world.tsx
@@ -16,27 +16,27 @@ export class WTFESM extends Post {
                 <h1>Hello, world!</h1>
 
                 <p>
-                    Allow me to introduce myself. I go by the name sundei, a 17-year-old student currently enrolled at Tartu Vocational College, where I am diligently working towards becoming an IT specialist. 
-                    My interests encompass a variety of activities, including gaming, following the <ExternalLink href="https://eurovision.tv">Eurovision Song Contest</ExternalLink>, playing the ukulele, 
-                    immersing myself in music, and cherishing moments of restful sleep. It's worth mentioning that I've been diagnosed with ADHD and anxiety, 
+                    Allow me to introduce myself. I go by the name sundei, a 17-year-old student currently enrolled at Tartu Vocational College, where I am diligently working towards becoming an IT specialist.
+                    My interests encompass a variety of activities, including gaming, following the <ExternalLink href="https://eurovision.tv">Eurovision Song Contest</ExternalLink>, playing the ukulele,
+                    immersing myself in music, and cherishing moments of restful sleep. It's worth mentioning that I've been diagnosed with ADHD and anxiety,
                     factors that shape some aspects of my life.
                 </p>
 
                 <h3>Why I did this blog?</h3>
 
                 <p>
-                    I embarked on this blogging journey without a predefined destination or purpose in mind. 
-                    My motivation was simple: a desire to articulate and share the myriad thoughts swirling within me, all while circumventing the complexities that often 
-                    accompany face-to-face conversations. As I navigated through the virtual realm of blogging, I discovered an outlet where my unfiltered ideas 
-                    could be unleashed and given room to breathe, free from the constraints of real-life interactions. 
-                    This platform granted me the liberty to explore the vast landscape of my mind and articulate the musings that might have otherwise remained dormant. 
-                    While my blog lacks a specific aim, it stands as a testament to the power of expression, providing me with a sanctuary where I can unabashedly 
+                    I embarked on this blogging journey without a predefined destination or purpose in mind.
+                    My motivation was simple: a desire to articulate and share the myriad thoughts swirling within me, all while circumventing the complexities that often
+                    accompany face-to-face conversations. As I navigated through the virtual realm of blogging, I discovered an outlet where my unfiltered ideas
+                    could be unleashed and given room to breathe, free from the constraints of real-life interactions.
+                    This platform granted me the liberty to explore the vast landscape of my mind and articulate the musings that might have otherwise remained dormant.
+                    While my blog lacks a specific aim, it stands as a testament to the power of expression, providing me with a sanctuary where I can unabashedly
                     lay bare the inner workings of my thoughts, fostering a unique form of introspective connection that transcends the boundaries of conventional communication.
                 </p>
 
                 <p>
-                    I have a diverse range of music styles and favorite artists that I enjoy. At the moment, one of my favorite artists is <ExternalLink href="https://alikamilova.ee">Alika Milova</ExternalLink>. 
-                    She represented Estonia in Eurovision and secured an impressive 8th place. Another artist I like is <ExternalLink href="https://jokeroutband.com">Joker Out</ExternalLink>, a band that represented Slovenia in Eurovision 2023. 
+                    I have a diverse range of music styles and favorite artists that I enjoy. At the moment, one of my favorite artists is <ExternalLink href="https://alikamilova.ee">Alika Milova</ExternalLink>.
+                    She represented Estonia in Eurovision and secured an impressive 8th place. Another artist I like is <ExternalLink href="https://jokeroutband.com">Joker Out</ExternalLink>, a band that represented Slovenia in Eurovision 2023.
                     Additionally, I'm a fan of <ExternalLink href="https://instagram.com/paidatonriehuja">Käärijä</ExternalLink>, our neighboring artist from Finland, and I was really hoping for Finland to win Eurovision this year.
                     Here are some playlists I'm currently enjoying:
                     <ul>
@@ -51,10 +51,10 @@ export class WTFESM extends Post {
                 <hr />
 
                 <p>
-                    I must admit that the year 2022/2023 was incredibly mentally exhausting for me. 
-                    I spent a lot of time vaping during that period, but I gradually started the process of quitting. 
+                    I must admit that the year 2022/2023 was incredibly mentally exhausting for me.
+                    I spent a lot of time vaping during that period, but I gradually started the process of quitting.
                     On a positive note, I managed to lose weight, which is a significant accomplishment.
-                    Additionally, I completed a year of High School, an experience that stands out as one of the most challenging in my educational journey. 
+                    Additionally, I completed a year of High School, an experience that stands out as one of the most challenging in my educational journey.
                     Despite the difficulties (and getting expelled), I did make a lot of new friends along the way.
                 </p>
             </>
