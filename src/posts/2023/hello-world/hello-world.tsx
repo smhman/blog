cc44481bdb327a373842b0dfdbc742bb50f53220
sundei <76597738+smhman@users.noreply.github.com>
2023-08-08 16:01:25 +0300

    Update hello-world.tsx

--- src/posts/2023/hello-world/hello-world.tsx
+++ src/posts/2023/hello-world/hello-world.tsx
@@ -1,21 +1,21 @@
-import {stripIndents} from 'common-tags';
-import {ExternalLink} from '../../../client/components/external-link';
-import {Post} from '../../Post';
+import { stripIndents } from 'common-tags';
+import { ExternalLink } from '../../../client/components/external-link';
+import { Post } from '../../Post';
 
 export class WTFESM extends Post {
-	public name = 'Hello, world';
-	public slug = 'hello-world';
-	public date = new Date('2023-08-08');
-	public hidden = false;
-	public excerpt = stripIndents`	
+    public name = 'Hello, world';
+    public slug = 'hello-world';
+    public date = new Date('2023-08-08');
+    public hidden = false;
+    public excerpt = stripIndents`
         Hello! This is my new blog
     `;
 
-	public keywords = ['hello', 'sundei', 'eurovision', 'alika', 'käärijä', 'joker-out', 'friends'];
+    public keywords = ['hello', 'sundei', 'eurovision', 'alika', 'käärijä', 'joker-out', 'friends'];
 
-	render() {
-		return (
-			<>
+    render() {
+        return (
+            <>
                 <h1>Hello, world!</h1>
 
                 <p>
@@ -25,7 +25,7 @@ export class WTFESM extends Post {
                     factors that shape some aspects of my life.
                 </p>
 
-				<h3>Why I did this blog?</h3>
+                <h3>Why I did this blog?</h3>
 
                 <p>
                     I embarked on this blogging journey without a predefined destination or purpose in mind.
@@ -37,9 +37,9 @@ export class WTFESM extends Post {
                     lay bare the inner workings of my thoughts, fostering a unique form of introspective connection that transcends the boundaries of conventional communication.
                 </p>
 
-				<h3>My music taste</h3>
-		
-				<p>
+                <h3>My music taste</h3>
+
+                <p>
                     I have a diverse range of music styles and favorite artists that I enjoy. At the moment, one of my favorite artists is <ExternalLink href="https://alikamilova.ee">Alika Milova</ExternalLink>.
                     She represented Estonia in Eurovision and secured an impressive 8th place. Another artist I like is <ExternalLink href="https://jokeroutband.com">Joker Out</ExternalLink>, a band that represented Slovenia in Eurovision 2023.
                     Additionally, I'm a fan of <ExternalLink href="https://instagram.com/paidatonriehuja">Käärijä</ExternalLink>, our neighboring artist from Finland, and I was really hoping for Finland to win Eurovision this year.
@@ -53,34 +53,34 @@ export class WTFESM extends Post {
                     My musical tastes are quite diverse, and these playlists capture just a portion of what I'm enjoying right now.
                 </p>
 
-				<hr />
-				<h3>About this year</h3>
-				<p>
+                <hr />
+                <h3>About this year</h3>
+                <p>
                     I must admit that the year 2022/2023 was incredibly mentally exhausting for me.
                     I spent a lot of time vaping during that period, but I gradually started the process of quitting.
                     On a positive note, I managed to lose weight, which is a significant accomplishment.
                     Additionally, I completed a year of High School, an experience that stands out as one of the most challenging in my educational journey.
                     Despite the difficulties (and getting expelled), I did make a lot of new friends along the way.
                 </p>
-				<h3>My special thanks</h3>
-				<p>
-				I am deeply grateful and feel compelled to express my heartfelt appreciation to a remarkable group of individuals who have played an essential role in guiding me through the intricate challenges that unfolded throughout this year. Among these outstanding individuals are Kasvi, Steph, Marie, GV, aapeh, Suomi, and several cherished friends from the realm of real life. 
-				Their unwavering support, encouragement, and camaraderie have been a beacon of light during moments when the path seemed obscured by shadows.
-				It is particularly noteworthy that some of these connections were forged later in the year, which only underscores the profound impact their presence has had on my well-being and journey. 
-				As I reflect upon the shared experiences, shared laughter, and the solace they offered in times of distress, I am reminded of the deep-seated bond that has been cultivated. 
-				Their collective empathy and willingness to stand by me during moments of vulnerability serve as a testament to the power of human connection.
-				In a world that can often feel fragmented and distant, the steadfast support of these remarkable individuals has illuminated the potential for meaningful relationships to flourish, even in the face of adversity. 
-				Their kindness, understanding, and genuine care have not only provided solace during trying times but have also enriched my life in immeasurable ways.
-				As I look back upon the year and contemplate the challenges that were surmounted, I am acutely aware that it is the presence of these exceptional individuals that has made all the difference. 
-				Their selflessness, empathy, and willingness to lend an ear or offer a helping hand have undoubtedly altered the trajectory of my experiences, transforming difficult moments into opportunities for growth and resilience.
-				In closing, my gratitude knows no bounds. To Kasvi, Steph, Marie, GV, aapeh, Suomi, and the dear friends who have stood by me, whether from the beginning of the year or later, your impact is immeasurable. 
-				Your unwavering support has been a source of strength, inspiration, and comfort, and for that, I am eternally thankful.
+                <h3>My special thanks</h3>
+                <p>
+                    I am deeply grateful and feel compelled to express my heartfelt appreciation to a remarkable group of individuals who have played an essential role in guiding me through the intricate challenges that unfolded throughout this year. Among these outstanding individuals are Kasvi, Steph, Marie, GV, aapeh, Suomi, and several cherished friends from the realm of real life.
+                    Their unwavering support, encouragement, and camaraderie have been a beacon of light during moments when the path seemed obscured by shadows.
+                    It is particularly noteworthy that some of these connections were forged later in the year, which only underscores the profound impact their presence has had on my well-being and journey.
+                    As I reflect upon the shared experiences, shared laughter, and the solace they offered in times of distress, I am reminded of the deep-seated bond that has been cultivated.
+                    Their collective empathy and willingness to stand by me during moments of vulnerability serve as a testament to the power of human connection.
+                    In a world that can often feel fragmented and distant, the steadfast support of these remarkable individuals has illuminated the potential for meaningful relationships to flourish, even in the face of adversity.
+                    Their kindness, understanding, and genuine care have not only provided solace during trying times but have also enriched my life in immeasurable ways.
+                    As I look back upon the year and contemplate the challenges that were surmounted, I am acutely aware that it is the presence of these exceptional individuals that has made all the difference.
+                    Their selflessness, empathy, and willingness to lend an ear or offer a helping hand have undoubtedly altered the trajectory of my experiences, transforming difficult moments into opportunities for growth and resilience.
+                    In closing, my gratitude knows no bounds. To Kasvi, Steph, Marie, GV, aapeh, Suomi, and the dear friends who have stood by me, whether from the beginning of the year or later, your impact is immeasurable.
+                    Your unwavering support has been a source of strength, inspiration, and comfort, and for that, I am eternally thankful.
                 </p>
-				<p>
-                    Thank you all and see you in next blog
-					~ sundei
+                <p>
+                    Thank you all and see you in the next blog
+                    ~ sundei
                 </p>
-			</>
-		);
-	}
+            </>
+        );
+    }
 }
