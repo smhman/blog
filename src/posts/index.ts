5ef8e0141bb2b6d7b3ce7d3bc20ec7b832470225
sundei <ajaleht@pg.edu.ee>
2023-09-16 20:01:21 +0300

    Update index.ts

--- src/posts/index.ts
+++ src/posts/index.ts
@@ -2,7 +2,6 @@ import {Hello} from './2023/hello/hello';
 import {Growtopia} from './2023/growtopia/growtopia';
 import {Alika} from './2023/alika-interview/alika-interview';
 
-
 export const posts = [
 	new Hello(),
 	new Growtopia(),
